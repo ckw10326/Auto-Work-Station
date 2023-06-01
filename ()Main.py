38873e62acaab7eb006463f8ee7710ce85e9f7d1
ckw10326 <81143251+ckw10326@users.noreply.github.com>
2023-06-01 16:12:42 +0800

    Update ()Main.py
    全域變數更動、可讀性最佳化

--- ()Main.py
+++ ()Main.py
@@ -8,23 +8,6 @@ import fnmatch
 import pandas as pd
 import datetime
 import pyxlsb
-# TC_abbreviation = ["TPC", "TC(C0))", "CD", "23"]
-# HT_abbreviation = ["HT", "D1", "CTC", "GEL", "23"]
-Easy_Dict = {"1":"HT", "2":"TC", "99":""}
-Plan_Dict = {"HT" : ["HT", "D1", "CTC", "GEL", "23"],
-             "TC" : ["TPC", "TC(C0)", "CD", "23"]
-             }
-Cloud_HT = r"/content/sample_data/HT"
-Cloud_TC = r"/content/sample_data/TC"
-Home_HT = r"C:\Users\OXO\OneDrive\01 Book\00 Test program\HT"
-Home_TC = r"C:\Users\OXO\OneDrive\01 Book\00 Test program\TC"
-Source_Company_HT = r"\\10.162.10.58\全處共用區\_Dwg\興達電廠燃氣機組更新計畫"
-destny_Company_HT = r"D:\00 興達計劃\05 EPC提供資料\HT"
-Source_Company_TC = r"\\10.162.10.58\全處共用區\_Dwg\台中電廠燃氣機組更新計畫"
-destny_Company_TC = r"D:\00 台中計劃\05 EPC提供資料\TC"
-Source_Customized = ""
-destny_Customized = ""
-
 #輸入 計畫名稱、末4號碼
 #輸出 計畫代號、完整文號
 def Get_DocNo():
@@ -71,13 +54,14 @@ def move_docutment(source_folder, dest_folder):
                 print("您選擇1，重新複製資料夾")
                 shutil.rmtree(dest_folder)
                 shutil.copytree(source_folder, dest_folder)
+                print("複製完成")
             else:
                 print("您選擇0，保留原本資料夾，不複製")
         else:
             shutil.copytree(source_folder, dest_folder)
     else:
         print("查「無」指定資料夾")
-    return dest_folder
+    return None
 def Read_HT_Excel(folder_path):
     #遍歷所有資料夾、檔案
     Excute_Num_block = []
@@ -250,7 +234,6 @@ def Read_TC_Excel(folder_path):
     #輸出成"Done_HT-D1-CTC-GEL-23-1171.xlsx"(範例)
     df.to_excel(Output_path, index=False)
     return Letter_titl_value, DrawinVision_value, Letter_Num_value, Letter_Date_value
-
 def Convert_xlsb(File_folder):
     for file in os.listdir(File_folder):
         if fnmatch.fnmatch(file, '*.xlsb'):
@@ -258,70 +241,101 @@ def Convert_xlsb(File_folder):
             print("找到檔案：", File_Path)
     #顯示表格
     df = pd.read_excel(File_Path, sheet_name='Data1', engine='pyxlsb')
-    Xlsx_File_Path = os.path.splitext(File_Path)[0] + "_converted.xlsx"
+    Xlsb_to_xlsx_Path = os.path.splitext(File_Path)[0] + "_converted.xlsx"
     #輸出檔案
-    df.to_excel(Xlsx_File_Path)
-    print("輸出檔案：", Xlsx_File_Path)
-    print("return ", File_folder)
-    input("暫停一下")
-    return File_folder
-
+    df.to_excel(Xlsb_to_xlsx_Path)
+    print("輸出檔案：", Xlsb_to_xlsx_Path)
+    return None
 def Text_Generator(Letter_titl_value, DrawinVision_value, Letter_Num_value, Letter_Date_value, PlanNo):
-    print(PlanNo)
+
     Plan_No = int(PlanNo)
     Doc_content = Letter_titl_value
     Doc_Vision = DrawinVision_value
     Doc_No = Letter_Num_value
     Doc_Date = Letter_Date_value
 
-    # PlanNo :  1 = 興達計畫, 2 = 台中計畫, 99 = 指定路徑
+    My_company_Num = str(input("請輸入審查意見填寫單位:"))
+    Pages = str(input("請輸入審查意見頁數:"))
     My_company = ["", "南部施工處", "中部施工處", "興達發電廠", "台中發電廠"]
     Consult_Company = ["", "吉興公司", "泰興公司", "GE/CTCI"]
-    Pages = str(input("請輸入審查意見頁數:"))
 
+    #複製套印文件
+    shutil.copy(src_path, dst_path)
+    
     date_obj = datetime.datetime.strptime(Doc_Date, "%Y/%m/%d")
     month = date_obj.strftime("%m")
     day = date_obj.strftime("%d")
     Plan_Name = "興達" if "CTC" in Doc_No else "台中"
 
-    contents0 = "本文係" + My_company[Plan_No] + "對統包商提送「" + Doc_content + "」" + "Rev." + str(Doc_Vision) +"所提審查意見(共" + Pages + "頁)" + "，未逾合約規範，已電傳" + Consult_Company[Plan_No] + "，擬陳閱後文存。"
-    contents1 = "檢送" + Plan_Name + "電廠燃氣機組更新改建計畫" + Doc_content + "Rev." + str(Doc_Vision) + "，" + My_company[Plan_No] + "之審查意見（如附，共" + Pages + "頁）供卓參，請查照。"
+    contents0 = "本文係" + My_company[My_company_Num] + "對統包商提送「" + Doc_content + "」" + "Rev." + str(Doc_Vision) +"所提審查意見(共" + Pages + "頁)" + "，未逾合約規範，已電傳" + Consult_Company[Plan_No] + "，擬陳閱後文存。"
+    contents1 = "檢送" + Plan_Name + "電廠燃氣機組更新改建計畫" + Doc_content + "Rev." + str(Doc_Vision) + "，" + My_company[My_company_Num] + "之審查意見（如附，共" + Pages + "頁）供卓參，請查照。"
     contents2 = "依據GE/CTCI 112年" + month + "月" + day +"日" + Doc_No + "號辦理。"
     contents4 = "本文係統包商提送「" + Doc_content + "」" + "Rev." + str(Doc_Vision) +"，本組無意見，已Email通知" + Consult_Company[Plan_No] + "公司" + "，擬陳閱後文存。"
     print("----------------他單位審查意見簽辦------------------")
     print(contents0)
     print("----------------傳真------------------")
     print(contents1)
-    #print(contents2)
+    print(contents2)
     print("----------------主辦簽辦------------------")
-    #print(contents4)
+    print(contents4)
     input("暫停")
     return 0
 
 def main():
+    Easy_Dict = {"1":"HT", "2":"TC", "99":""}
+    Plan_Dict = {"HT" : ["HT", "D1", "CTC", "GEL", "23"],
+                 "TC" : ["TPC", "TC(C0)", "CD", "23"]
+                 }
+    Cloud_HT = r"/content/sample_data/HT"
+    Cloud_TC = r"/content/sample_data/TC"
+    Home_HT = r"C:\Users\OXO\OneDrive\01 Book\00 Test program\HT"
+    Home_TC = r"C:\Users\OXO\OneDrive\01 Book\00 Test program\TC"
+    #來源資料夾、目標資料夾
+    Source_Company_HT = r"\\10.162.10.58\全處共用區\_Dwg\興達電廠燃氣機組更新計畫"
+    destny_Company_HT = r"D:\00 興達計劃\05 EPC提供資料\HT"
+    Source_Company_TC = r"\\10.162.10.58\全處共用區\_Dwg\台中電廠燃氣機組更新計畫"
+    destny_Company_TC = r"D:\00 台中計劃\05 EPC提供資料\TC"
+    Source_Customized = ""
+    destny_Customized = ""
+    PlanNo = ""#計畫代碼
+    DocNo = ""#廠商文號
+    source_folder = ""#資料來源資料夾
+    dest_folder = ""#存放路徑資料夾
+    Xlsb_to_xlsx_Path = ""#轉檔路徑
+    Letter_Title = ""#來文資訊
+    Drawing_Vision = ""#來文資訊
+    Letter_Num = ""#來文資訊
+    Letter_Date = ""#來文資訊
+    Pring_page_StandardFile = r"D:\00 興達計劃\HT套印標準.rtf"#套印檔案路徑
+    Print_page_File = ""
+    
     PlanNo, DocNo = Get_DocNo()
-    print("PlanNo:", PlanNo)
-    print("DocNo:", DocNo)
+    print("PlanNo計畫代碼:", PlanNo)
+    print("DocNo廠商文號:", DocNo)
     print(r"----------------------Get_DocNo Done!-------------------------------------------")
+    
     source_folder, dest_folder= Gen_path(PlanNo, DocNo)
-    print("source_folder:", source_folder)
-    print("dest_folder:", dest_folder) 
+    print("source_folder資料來源資料夾:", source_folder)
+    print("dest_folder存放路徑資料夾:", dest_folder) 
     print(r"----------------------Gen_path Done!--------------------------------------------")
-    dest_folder = move_docutment(source_folder, dest_folder)
-    print("dest_folder:",dest_folder)
+    
+    move_docutment(source_folder, dest_folder)
     print(r"----------------------move_docutment Done!--------------------------------------")
     #開啟複製好的資料價
     os.startfile(dest_folder)
+    
     if "TC(C0)" in dest_folder:
-        Letter_titl_value, DrawinVision_value, Letter_Num_value, Letter_Date_value = Read_TC_Excel(dest_folder)
+        Letter_Title, Drawing_Vision, Letter_Num, Letter_Date = Read_TC_Excel(dest_folder)
     else:
         #若為興達計畫，則額外執行.xlsb轉換.xlsx動作
-        Xlsx_File_Path = Convert_xlsb(dest_folder)
-        print(r"----------------------Convert_xlsb Done!--------------------------------------")
-        Letter_titl_value, DrawinVision_value, Letter_Num_value, Letter_Date_value = Read_HT_Excel(dest_folder)
+        Convert_xlsb(dest_folder)
+        print(r"----------------------Convert_xlsb Done!-----------------------------------")
+        Letter_Title, Drawing_Vision, Letter_Num, Letter_Date = Read_HT_Excel(dest_folder)
     print("來文名稱：", Letter_titl_value, "\n版次：" , DrawinVision_value, "\n來文號碼：", Letter_Num_value,"\n來文日期：", Letter_Date_value )
     print(r"----------------------Read_Excel Done!------------------------------------------")   
-    Text_Generator(Letter_titl_value, DrawinVision_value, Letter_Num_value, Letter_Date_value, PlanNo)
+    
+    Print_page_File = dest_folder + r"\套印_" + Doc_No + ".rtf"
+    Text_Generator(Letter_Title, Drawing_Vision, Letter_Num, Letter_Date, PlanNo)
     print(r"----------------------Text_Generator Done!------------------------------------------") 
     input("Press enter to exit...")
 
